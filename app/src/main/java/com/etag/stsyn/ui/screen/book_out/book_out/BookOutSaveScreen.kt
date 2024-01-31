5eabac9052d331b4025b750ddefc4551542d9437
Hlwam_Moe_Naing <53623355+HlwamMoeNaing@users.noreply.github.com>
2024-01-31 16:55:04 +0630

    Fix: Infinite dialog show when api error happen in BookOutSaveScreen.kt

--- app/src/main/java/com/etag/stsyn/ui/screen/book_out/book_out/BookOutSaveScreen.kt
+++ app/src/main/java/com/etag/stsyn/ui/screen/book_out/book_out/BookOutSaveScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddLocation
-import androidx.compose.material.icons.filled.Error
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.outlined.TrackChanges
 import androidx.compose.material3.Text
@@ -17,7 +16,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -25,7 +23,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.etag.stsyn.enums.Purpose
-import com.etag.stsyn.ui.components.CustomIcon
 import com.etag.stsyn.ui.components.DropDown
 import com.etag.stsyn.ui.components.LoadingDialog
 import com.etag.stsyn.ui.components.STSYNTExtField
